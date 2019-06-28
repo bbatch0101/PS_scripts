f1ac2a3dff0e8df3fe72db2dcdee32442741f59b
bbatch0101 <49376035+bbatch0101@users.noreply.github.com>
2019-06-28 11:17:22 -0500

    Update Clear_Cache.ps1

--- Clear_Cache.ps1
+++ Clear_Cache.ps1
@@ -1,1 +1,13 @@
+$CompName = Read-Host "Please enter IP or Host name"
+
+
+$INetCookies = "\\$CompName\c$\Users\POC0567\AppData\Local\Packages\KohlsPOCClient_bbqcjhznsstxt\AC\INetCookies\*.*"
+$INetHistory = "\\$CompName\c$\Users\POC0567\AppData\Local\Packages\KohlsPOCClient_bbqcjhznsstxt\AC\INetHistory\*.*"
+$INetCache   = "\\$CompName\c$\Users\POC0567\AppData\Local\Packages\KohlsPOCClient_bbqcjhznsstxt\AC\INetCache\*.*"
+
+if (Get-Process -ComputerName $CompName Kohls.PointOfCommerce.WinRT.Industry -eq "Running") {
+    Stop-Process Kohls.PointOfCommerce.WinRT.Industry
+}
+
+Get-ChildItem -Path $INetCookies, $INetHistory, $INetCache | Remove-Item -Recurse
 
